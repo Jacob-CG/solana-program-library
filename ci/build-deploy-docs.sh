dc6e2f089dee7f790b0bafef89fcfaadb31db7c4
cryptogosu <github@cryptogosu.com>
2021-10-29 15:52:42 -0500

    Updated typedocs CICD

--- ci/build-deploy-docs.sh
+++ ci/build-deploy-docs.sh
@@ -6,24 +6,16 @@ cwd=$(pwd)
 
 cd ${cwd}/token/js
 npm install
-npm run build
 npm run docs
 
 cd ${cwd}/token-lending/js
 npm install
-npm run build
 npm run docs
 
 cd ${cwd}/stake-pool/js
 npm install
-npm run build
 npm run docs
 
 cd ${cwd}/token-swap/js
 npm install
-npm run build
 npm run docs
-
-cd ${cwd}
-npm install
-npm run deploy
